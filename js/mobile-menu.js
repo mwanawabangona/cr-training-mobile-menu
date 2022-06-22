ab18818f701c1299ed5d7f5e2b06fc4278c7f154
mwanawabangona <ellisallday@gmail.com>
2022-06-22 07:51:05 -0400

    Add JS to hover menu

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,29 +1,29 @@
 document.querySelector('.menu-bars').addEventListener('click', () => {
   document.getElementById('desktop-nav').style.cssText = `
-    display: flex;
-    justify-content: space-around;
-    z-index: 50;
-    background-color: inherit;
-    position: fixed;
-    height: 100vh;
-    width: 100vw;
-    top: 0;
-    left: 0;
-    `;
+      display: flex;
+      justify-content: space-around;
+      z-index: 50;
+      background-color: inherit;
+      position: fixed;
+      height: 100vh;
+      width: 100vw;
+      top: 0;
+      left: 0;
+      `;
 
   const list = document.querySelector('#desktop-nav > ul');
   list.style.cssText = `
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    `;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      `;
 });
 
 document.querySelectorAll('.nav-link').forEach((el) => {
   el.addEventListener('click', () => {
     document.getElementById('desktop-nav').style.cssText = `
-        display: none;
-        `;
+          display: none;
+          `;
   });
   // eslint-disable-next-line no-console
   console.log(el);
